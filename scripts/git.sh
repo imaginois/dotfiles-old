1e78dff6ded56c8e0c60770e72a8c09a09f57a80
Metodi Deribanov <imaginois@gmail.com>
2017-12-18 18:47:03 -0800

    add sublime project files

--- scripts/git.sh
+++ scripts/git.sh
@@ -4,19 +4,21 @@ echo "## Running git.sh script"
 
 # Setup git config stuff
 
-# set global .gitignore
-git config --global core.excludesfile '~/.gitignore'
-
 echo "### Setup global ~/.gitignore"
-if [ -f ~/.gitignore ]; then
+if [ -f ~/.gitignore ] || [ -L ~/.gitignore ]; then
   rm -f ~/.gitignore
 fi
 
 ln -s $DOT_HOME/git/gitignore_global.symlink ~/.gitignore
 
 echo "### Setup global ~/.gitconfig"
-if [ -f ~/.gitconfig ]; then
+if [ -f ~/.gitconfig ] || [ -L ~/.gitconfig ]; then
   rm -f ~/.gitconfig
 fi
 
 ln -s $DOT_HOME/git/gitconfig.symlink ~/.gitconfig
+
+
+
+# set global .gitignore
+git config --global core.excludesfile '~/.gitignore'
