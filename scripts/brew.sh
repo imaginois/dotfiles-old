5cfa390c5dce41f121f43aa25b3cf7cd57dd7e02
Metodi Deribanov <imaginois@gmail.com>
2017-12-19 14:07:01 -0800

    add Docker for Mac

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+# Install Mac Specific apps but not with homebrew
+# Install Docker
+echo "\n ### Installing Docker"
+wget https://download.docker.com/mac/stable/Docker.dmg
+mv Docker.dmg /Applications/
+
 #install zsh and zsh-completions
 brew install zsh zsh-completions
